6c2b79ea991c73c73a485c19fccb06817b553aa7
Luca Leon Happel <lucahappel99@gmx.de>
2025-01-02 22:43:27 +0100

    add: initial esp32s3 application setup

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "My development shell for all my RV projects";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/flake-utils";
+    };
+    esp32.url = "github:mirrexagon/nixpkgs-esp-dev";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, esp32 }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import "${esp32}/overlay.nix") ];
+        };
+      in rec {
+        defaultPackage = packages.wohnmobil;
+        packages.wohnmobil = with pkgs;
+          mkShell {
+            buildInputs = [ esp-idf-full ];
+
+            shellHook = ''
+              source ${esp-idf-full}/export.sh
+            '';
+
+          };
+      });
+}
